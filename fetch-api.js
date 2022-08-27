763aa941c9348f28c9f0355be584d3e746fdbaf3
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-08-27 09:44:20 +0600

    third commit

---
+++ fetch-api.js
@@ -0,0 +1,10 @@
+
+
+
+function loadData() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
